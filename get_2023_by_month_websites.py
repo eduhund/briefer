76ab6b6ea75df36a7d2e392636713ad45e6ca0f6
Olga Pavlova <olgapavlova@gmail.com>
2023-07-26 12:01:07 +0300

    some jan 2003 websites

---
+++ get_2023_by_month_websites.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from generate_date_interval import generate_date_interval
+from save_websites_for_launch_date import save_websites_for_launch_date
+
+interval = generate_date_interval(datetime(2023, 1, 25), datetime(2023, 1, 31))
+
+for day in interval:
+    save_websites_for_launch_date(day)
