034d308435a0de63d50c4e827d0b890a41957cd5
Olga Pavlova <olgapavlova@gmail.com>
2023-07-26 17:25:42 +0300

    load websites dataframe from file

---
+++ load_websites_dataframe_from_file.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from datetime import datetime
+
+def load_websites_dataframe_from_file(date):
+    """Получить датафрейм конкретного дня из файла"""
+    # TODO Убрать константу из кода
+    WEBSITES_ARCHIVE = 'websites/' 
+
+    # TODO Убрать вложенность? Вообще криво как-то с этим сохранением
+    ldf = pd.read_pickle(f"{WEBSITES_ARCHIVE}{date.year}-{date.month}-{date.day}.pkl")
+
+    return ldf
+
+if __name__ == '__main__':
+    print(load_websites_dataframe_from_file(datetime(2023,3,31)))
