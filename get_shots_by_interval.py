fa97d550f429f8c85c9255eda135326740309add
Olga Pavlova <olgapavlova@gmail.com>
2023-07-30 12:13:49 +0300

    download shots dates

--- get_shots_by_interval.py
+++ get_shots_by_interval.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from generate_date_interval import generate_date_interval
 from save_shots_for_launch_day import save_shots_for_launch_day
 
-interval = generate_date_interval(datetime(2023, 5, 20), datetime(2023, 7, 25))
+interval = generate_date_interval(datetime(2023, 6, 30), datetime(2023, 7, 25))
 
 for day in interval:
     print(day)
